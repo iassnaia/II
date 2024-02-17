bbd39a99fa326e4182bb6ea07de29bad46074cfb
iassnaia <110601635+iassnaia@users.noreply.github.com>
2024-02-17 21:09:05 +0300

    Update DZ6.py

--- DZ6.py
+++ DZ6.py
@@ -1,79 +1,35 @@
-import telebot 
+import psysuperbot 
 from random import *
 import json
 import requests
 films = []
 API_URL='https://7012.deeppavlov.ai/model'
 def save():
-    with open("films.json","w",encoding="utf-8") as fh:
-        fh.write(json.dumps(films,ensure_ascii=False))
-    print("Наша фильмотека была успешно сохранена в файле films.json")
+    with open("tests.json","w",encoding="utf-8") as fh:
+        fh.write(json.dumps(tests,ensure_ascii=False))
+    print("Наши тесты были успешно сохранены в файле tests.json")
 def load():
     global films
-    with open("films.json","r",encoding="utf-8") as fh:
-        films=json.load(fh)
-    print("Фильмотека была успешно загружена")
+    with open("tests.json","r",encoding="utf-8") as fh:
+        tests=json.load(fh)
+    print("Тесты были успешно загружены")
 
 API_TOKEN = "6737709947:AAHq13bzIkVAtTTb1D0aNobROD_CyNTrAlM"
-bot = telebot.TeleBot(API_TOKEN)
+bot = psysuperbot.PsysuperBot(API_TOKEN)
 
 @bot.message_handler(commands=["start"])
 
 def start_message(message):
-    films.append("Матрица")
-    films.append("Солярис")
-    films.append("Властелин колец")
-    films.append("Пила")
-    films.append("Санта Барбара")
-    bot.send_message(message.chat.id,"Фильмотека была загружена по умолчанию")
+   tests.append("Депрессия")
+    tests.append(" Невроз")
+    tests.append("Тревожность")
+    tests.append("Зависимость")
+    tests.append("Психические расстройства")
+    bot.send_message(message.chat.id," Тесты были загружены по умолчанию")
 @bot.message_handler(commands=['all'])
 def show_all(message):
-    bot.send_message(message.chat.id,"Вот список фильмов")
-    bot.send_message(message.chat.id, ", ".join(films))
-
-@bot.message_handler(commands=['wiki'])
-def wiki(message):
-    quest = message.text.split()[1(inlove) 
-    qq=" ".join(quest)
-    data = { 'question_raw': [qq]}
-    try:
-        res = requests.post(API_URL,json=data,verify=False).json()
-        bot.send_message(message.chat.id, res)
-    except:
-        bot.send_message(message.chat.id, "Что-то я ничего не нашел :-(")
-
-bot.polling()import telebot 
-from random import *
-import json
-import requests
-films = []
-API_URL='https://7012.deeppavlov.ai/model'
-def save():
-    with open("films.json","w",encoding="utf-8") as fh:
-        fh.write(json.dumps(films,ensure_ascii=False))
-    print("Наша фильмотека была успешно сохранена в файле films.json")
-def load():
-    global films
-    with open("films.json","r",encoding="utf-8") as fh:
-        films=json.load(fh)
-    print("Фильмотека была успешно загружена")
-
-API_TOKEN = "6737709947:AAHq13bzIkVAtTTb1D0aNobROD_CyNTrAlM"
-bot = telebot.TeleBot(API_TOKEN)
-
-@bot.message_handler(commands=["start"])
-
-def start_message(message):
-    films.append("Матрица")
-    films.append("Солярис")
-    films.append("Властелин колец")
-    films.append("Пила")
-    films.append("Санта Барбара")
-    bot.send_message(message.chat.id,"Фильмотека была загружена по умолчанию")
-@bot.message_handler(commands=['all'])
-def show_all(message):
-    bot.send_message(message.chat.id,"Вот список фильмов")
-    bot.send_message(message.chat.id, ", ".join(films))
+    bot.send_message(message.chat.id,"Вот список тестов")
+    bot.send_message(message.chat.id, ", ".join(tests))
 
 @bot.message_handler(commands=['wiki'])
 def wiki(message):
